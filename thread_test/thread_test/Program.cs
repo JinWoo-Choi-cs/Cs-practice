6da3d795dee2026c22b7c2acaaac0d0cd0bc3f37
Unknown <wlsdngjwjq@gmail.com>
2018-11-26 11:16:09 +0900

    : fetch create class

--- thread_test/thread_test/Program.cs
+++ thread_test/thread_test/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace thread_test
@@ -10,6 +11,41 @@ namespace thread_test
     {
         static void Main(string[] args)
         {
+            TestT tt = new TestT();
+            Thread thread = new Thread(new ThreadStart(tt.run));
+
+            thread.Start();
+            while (true)
+            {
+                Console.Write("입력 : ");
+                TestT.count = int.Parse( Console.ReadLine());
+
+                if (thread.ThreadState == ThreadState.Aborted && TestT.count == 5)
+                    thread.Resume();
+            }
+        }
+
+
+
+    }
+
+    class TestT
+    {
+        public static int count = 0;
+        public void run()
+        {
+
+            while (true)
+            {
+                Console.WriteLine($"run의 카운트 = {count}");
+                Thread.Sleep(1000);
+
+
+                if (count == 3)
+                    Thread.CurrentThread.();
+            }
         }
     }
+
+
 }
